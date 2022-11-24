40ebeea2c6e5230e5c2c153183bb1c7347454e7e
Carrot <11172391+CRThu@users.noreply.github.com>
2022-11-24 23:23:35 +0800

    CoreReader单元测试更新

---
+++ CAMLTests/Impl/CodeReaderTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAML.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAML.Impl.Tests
+{
+    [TestClass()]
+    public class CodeReaderTests
+    {
+
+        [TestMethod()]
+        public void NextTest()
+        {
+            CodeReader cr = new("123\nHelloworld");
+            Assert.AreEqual(cr.Next(), '1');
+            Assert.AreEqual(cr.Next(), '2');
+            Assert.AreEqual(cr.Next(), '3');
+            Assert.AreEqual(cr.Cursor, 3);
+        }
+
+        [TestMethod()]
+        public void GetCharTest()
+        {
+            CodeReader cr = new("123\nHelloworld");
+            cr.Cursor = 4;
+            Assert.AreEqual(cr.GetChar(), 'H');
+            cr.Cursor = 5;
+            Assert.AreEqual(cr.GetChar(), 'e');
+            cr.Cursor = 6;
+            Assert.AreEqual(cr.GetChar(), 'l');
+        }
+
+        [TestMethod()]
+        public void GetNextCharTest()
+        {
+            CodeReader cr = new("123\nHelloworld");
+            cr.Cursor = 4;
+            Assert.AreEqual( cr.GetNextChar(),'H');
+            cr.Cursor = 5;
+            Assert.AreEqual(cr.GetNextChar(3), 'o');
+        }
+
+        [TestMethod()]
+        public void HasNextTest()
+        {
+            CodeReader cr = new("123\nHelloworld");
+            cr.Cursor = 6;
+            Assert.AreEqual(cr.HasNext(), true);
+            cr.Cursor = 13;
+            Assert.AreEqual(cr.HasNext(), true);
+            cr.Cursor = 14;
+            Assert.AreEqual(cr.HasNext(), false);
+        }
+    }
+}
