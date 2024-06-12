f5e0be8a2825d6a74023f069566ea0d8b7ae2c2d
Carrot <11172391+CRThu@users.noreply.github.com>
2024-06-12 23:27:04 +0800

    单元测试代码更新

---
+++ CarrotScriptTests/LexarExprTests.cs
@@ -0,0 +1,91 @@
+﻿using CarrotScript.Lexar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarrotScript.Lang.Def;
+
+namespace CarrotScriptTests
+{
+    [TestClass()]
+    public class LexarExprTests
+    {
+        [TestMethod()]
+        public void ParseExprStringTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "ABC");
+            Lexar lexar = new(codeReader);
+            var tokens = lexar.Parse();
+
+            Assert.AreEqual(1, tokens.Count);
+            Assert.AreEqual("ABC", tokens[0].Value);
+            Assert.AreEqual(TokenType.STRING, tokens[0].Type);
+        }
+
+        [TestMethod()]
+        public void ParseExprNumIntPosTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "123");
+            Lexar lexar = new(codeReader);
+            var tokens = lexar.Parse();
+
+            Assert.AreEqual(1, tokens.Count);
+            Assert.AreEqual(123, tokens[0].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[0].Type);
+        }
+
+        [TestMethod()]
+        public void ParseExprNumIntNegTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "-123");
+            Lexar lexar = new(codeReader);
+            var tokens = lexar.Parse();
+
+            Assert.AreEqual(1, tokens.Count);
+            Assert.AreEqual(-123, tokens[0].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[0].Type);
+        }
+
+        [TestMethod()]
+        public void ParseExprAddTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "1+3");
+            Lexar lexar = new(codeReader);
+            var tokens = lexar.Parse();
+
+            Assert.AreEqual(3, tokens.Count);
+            Assert.AreEqual(1, tokens[0].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[0].Type);
+            Assert.AreEqual("+", tokens[1].Value);
+            Assert.AreEqual(TokenType.ADD, tokens[1].Type);
+            Assert.AreEqual(3, tokens[2].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[2].Type);
+        }
+
+        [TestMethod()]
+        public void ParseExprMulTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "1*3");
+            Lexar lexar = new(codeReader);
+            var tokens = lexar.Parse();
+
+            Assert.AreEqual(3, tokens.Count);
+            Assert.AreEqual(1, tokens[0].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[0].Type);
+            Assert.AreEqual("*", tokens[1].Value);
+            Assert.AreEqual(TokenType.MUL, tokens[1].Type);
+            Assert.AreEqual(3, tokens[2].Value);
+            Assert.AreEqual(TokenType.NUMERIC, tokens[2].Type);
+        }
+
+        [TestMethod()]
+        public void ParseExprComplexTest()
+        {
+            CodeReader codeReader = new CodeReader("<FILE>", "2**3\t+ 7.9E2+7*E6/-.32+(1+2*_x_)*-5");
+            Lexar lexar = new(codeReader);
+            lexar.Parse();
+        }
+    }
+}
