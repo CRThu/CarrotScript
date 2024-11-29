e26208f20157f2dfc363af9701848ecd7f4ad3ac
Carrot <11172391+CRThu@users.noreply.github.com>
2024-11-29 17:02:16 +0800

    update RuntimeEnvironment

---
+++ CarrotScript/Interpreter/RuntimeEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotScript.Interpreter
+{
+    public class RuntimeEnvironment
+    {
+        private readonly Dictionary<string, double> variables = new();
+
+        public void SetVariable(string name, double value)
+        {
+            variables[name] = value;
+        }
+
+        public double GetVariable(string name)
+        {
+            if (!variables.ContainsKey(name))
+            {
+                variables[name] = 0;
+            }
+            return variables[name];
+        }
+
+        public void PrintSnapshot()
+        {
+            Console.WriteLine("--- RuntimeEnvironment ---");
+            foreach (var key in variables.Keys)
+            {
+                Console.WriteLine($"{key}={variables[key]}");
+            }
+        }
+    }
+}
